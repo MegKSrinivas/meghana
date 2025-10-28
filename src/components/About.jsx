4343510c51bfb6affb2109e8e2c2b1daa5015ba2
MegKSrinivas <meghanaksrinivas@gmail.com>
2025-10-28 00:43:52 -0400

    home and about basic struct

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,53 @@
+import KeyboardDoubleArrowRightOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowRightOutlined';
+import { Grid } from '@mui/material';
+import Box from '@mui/material/Box';
+
+import '../styles/About.css';
+
 export default function About() {
-  return <div>'About'</div>;
+  let skillList = ['Skill 1', 'Skill 2', 'Skill 3'];
+  return (
+    <div className="about">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={4}>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <div className="about_section">
+              <h4> about me</h4>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  condimentum, neque at bibendum finibus, lectus nibh cursus
+                  velit, a dictum justo dui eget lorem. Curabitur venenatis,
+                  arcu vitae maximus suscipit, turpis massa malesuada sem, vitae
+                  fermentum nisi sapien at risus.
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+                {skillList.map((item) => {
+                  return (
+                    <div>
+                      <span>
+                        <KeyboardDoubleArrowRightOutlinedIcon
+                          sx={{
+                            color: 'var(--color-accent, #f1a9d6);',
+                            paddingRight: '2rem',
+                          }}
+                        />
+                        {item}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <div>
+              <h3>*Space for photo*</h3>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>'Home'</div>;
+import '../styles/Home.css';
+
+export default function HomePage() {
+  return (
+    <section className="home">
+      <h1 className="home_title">
+        <span className="home_typing">
+          hi,&nbsp;<span className="accent">meghana</span>&nbsp;here.
+        </span>
+      </h1>
+
+      <p className="home_paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+        condimentum, neque at bibendum finibus, lectus nibh cursus velit, a
+        dictum justo dui eget lorem. Curabitur venenatis, arcu vitae maximus
+        suscipit, turpis massa malesuada sem, vitae fermentum nisi sapien at
+        risus.
+      </p>
+    </section>
+  );
 }
